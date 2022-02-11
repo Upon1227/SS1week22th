f1907cd658d3ab97396421e5da7d1a49a6f3e0d5
Sadaaki1104 <60700157+Sadaaki1104@users.noreply.github.com>
2022-02-11 19:00:55 +0900

    スコア表示

--- Assets/sadaaki/Scripts/MainClock.cs
+++ Assets/sadaaki/Scripts/MainClock.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainClock : MonoBehaviour
 {
     public static float Timer = 0.0F;
+    public float timercopy = 0.0F;
     public static int Clock = 9;
     public Text ClockText;
-
+    creditManager creditManager;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +24,21 @@ public class MainClock : MonoBehaviour
     {
         ClockText.text = Clock + ":00";
         Timer += Time.deltaTime;
+        timercopy += Time.deltaTime;
         if (Timer>37.5)
         {
             Timer = 0;
+            timercopy = 0;
             Clock += 1;
         }
+
+        if(Clock == 17)
+        {
+            SceneManager.LoadScene("MainSlot5");
+            creditManager = GameObject.Find("credit").GetComponent<creditManager>();
+            float Score = creditManager.credit;
+            naichilab.RankingLoader.Instance.SendScoreAndShowRanking(Score);
+        }
+
     }
 }
